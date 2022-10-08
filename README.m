a9d6102bbdf0d4f8e0ef6f1b701dcbc2843e0fdf
Romeo2801 <115368120+Romeo2801@users.noreply.github.com>
2022-10-08 23:09:54 +0530

    README.BIO
    Bio

---
+++ README.m
@@ -0,0 +1,7 @@
+- 👋 Hi, I’m @Romeo2801
+- 👀 I’m interested in Data Science. 
+- 🌱 I’m currently learning from scratch. 
+- 💞️ I’m looking to collaborate on anything useful and fun! 
+- 📫 How to reach me. Well, you can't reach me but yeah you can DM me on insta @_other_half_empty. 
+ 
+
